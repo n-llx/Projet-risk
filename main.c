4911fa60c489276c7fa0f9b4b622053ad3dd5163
adrien-laratte <93127380+adrien-laratte@users.noreply.github.com>
2022-01-09 18:22:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -15,5 +15,34 @@ int main(void){
 
   InfoCase information_territoires[6];
   initialisation_jeu(3, 6, grille_jeu, liste_territoire, grille_jeu, information_territoires);
+  
+  
+  
+  
+  char tour_du_joueur_n = 'A';
+
+  while(sb_win_fun(information_territoires) == false){
+    
+    printf ("C'est au joueur %c de jouer !\n", tour_du_joueur_n);
+
+    fonction_renfort(information_territoires, tour_du_joueur_n);
+
+    
+    int desir_combat = desir_de_combat();
+     
+    if (desir_combat == 1){
+      printf ("Qui voulez-vous attaquer ? Veuillez renseigner une lettre entre A et F sauf vous-même");
+      char defenseur;
+      scanf ("%c", &defenseur);
+      fonction_combat(information_territoires[tour_du_joueur_n - 'A'], information_territoires[defenseur - 'A'], 0, matrice_adjacence, grille_jeu);
+    }
+    
+    tour_du_joueur_n =+ 1;
+
+    if (tour_du_joueur_n == 'G'){
+      tour_du_joueur_n = 'A';
+    }
+  }
+  
   return EXIT_SUCCESS;
 }
