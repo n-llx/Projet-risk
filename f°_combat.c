df716b6b7e69246f7967db3ca4cfd3759608b862
Nils <94458864+n-llx@users.noreply.github.com>
2022-01-09 13:58:40 +0100

    Update f°_combat.c

--- f°_combat.c
+++ f°_combat.c
@@ -4,12 +4,12 @@
 #include "fonction.c"
 
 
-int fonction_combat(int tab_attaquant[], int tab_defenseur[], int phase, int matrice_du_graphe[4][4]){
+int fonction_combat(InfoCase attaquant, InfoCase defenseur, int phase, int matrice_du_graphe[6][6]){
     
     // La phase 0 correspond à la toute première attaque du tour
     if (phase == 0){
     
-        int info = autorise_combat(4, matrice_du_graphe, tab_attaquant[1], tab_defenseur[1], tab_attaquant[0], tab_defenseur[0]);
+        int info = autorise_combat(4, matrice_du_graphe, attaquant.nom_de_la_case, defenseur.nom_de_la_case, attaquant.nombre_de_troupe, defenseur.nombre_de_troupe);
 
         if (informe_joueur(info, 0) == 1) {
             return -1;
@@ -17,15 +17,15 @@ int fonction_combat(int tab_attaquant[], int tab_defenseur[], int phase, int mat
     }
     
     int tab_perte_troupe[2] = {0,0};
-    un_combat(tab_attaquant[0], tab_defenseur[0], tab_perte_troupe);
+    un_combat(attaquant.nombre_de_troupe, defenseur.nombre_de_troupe, tab_perte_troupe);
 
-    tab_attaquant[0] += tab_perte_troupe[0];
-    tab_defenseur[0] += tab_perte_troupe[1];
+    attaquant.nombre_de_troupe += tab_perte_troupe[0];
+    defenseur.nombre_de_troupe += tab_perte_troupe[1];
 
     printf ("Perte en troupe de l'attaquant : %d\n", tab_perte_troupe[0]);
     printf ("Perte en troupe du défenseur : %d\n", tab_perte_troupe[1]);
 
-    int info_2 = autorise_combat(4, matrice_du_graphe, tab_attaquant[1], tab_defenseur[1], tab_attaquant[0], tab_defenseur[0]);
+    int info_2 = autorise_combat(4, matrice_du_graphe, attaquant.nom_de_la_case, defenseur.nom_de_la_case, attaquant.nombre_de_troupe, defenseur.nombre_de_troupe);
 
     if (informe_joueur(info_2, 1) == 1) {
         return -1;
@@ -40,7 +40,7 @@ int fonction_combat(int tab_attaquant[], int tab_defenseur[], int phase, int mat
     fonction_affichage_plateau(40, 182, carte, 0, 0, des);
 
     if (desision == 1){
-        fonction_combat(tab_attaquant, tab_defenseur, 1, matrice_du_graphe);
+        fonction_combat(attaquant, defenseur, 1, matrice_du_graphe);
     }
     else {
         return -2;
